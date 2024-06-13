c48bb05d530811db3f9bc42d57123e16fd3665bd
anass2002-dr <anassdermaj163@gmail.com>
2024-06-13 23:11:06 +0100

    13-06-2024-23-10

--- application/app/Utilisateur/ListUtilisateur/page.tsx
+++ application/app/Utilisateur/ListUtilisateur/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function ListUtilisateur() {
@@ -8,7 +8,6 @@ export default function ListUtilisateur() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Replace with your actual API endpoint
     fetch("https://localhost:7019/api/Users/GetUsers")
       .then((response) => response.json())
       .then((data) => {
@@ -24,32 +23,95 @@ export default function ListUtilisateur() {
   if (loading) {
     return <div>Loading...</div>;
   }
-  const deleteUser = (idUserToDelete:any) => {
+  const deleteUser = (idUserToDelete: any) => {
+    const confirmed = window.confirm(
+      "Êtes-vous sûr de vouloir supprimer cette Utilisateur"
+    );
+    if (!confirmed) return;
+
     fetch(`https://localhost:7019/api/Users/DeleteUser?id=${idUserToDelete}`, {
-    method: "DELETE", // Use 'DELETE' for deletion
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .then((data) => {
-      if (data) {
-        console.log("User deleted successfully");
-        setUtilisateurs(utilisateurs.filter((user:any) => user.idUser !== idUserToDelete));
-      } else {
-        console.error("User deletion failed");
-        // Handle deletion failure (e.g., display an error message)
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      // Handle network errors or other exceptions
-    });
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data) {
+          console.log("User deleted successfully");
+          setUtilisateurs(
+            utilisateurs.filter((user: any) => user.idUser !== idUserToDelete)
+          );
+        } else {
+          const confirmed2 = window.confirm(
+            "les tâches de ce utilisateur sera aussi supprimer"
+          );
+          if (!confirmed2) return;
+          fetch(
+            `https://localhost:7019/api/Taches/DeleteTacheByUserId?id=${idUserToDelete}`,
+            {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          )
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Network response was not ok");
+              }
+              return response.json();
+            })
+            .then((data) => {
+              if (data) {
+                console.log("Tache deleted successfully");
+                fetch(
+                  `https://localhost:7019/api/Users/DeleteUser?id=${idUserToDelete}`,
+                  {
+                    method: "DELETE",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                  }
+                )
+                  .then((response) => {
+                    if (!response.ok) {
+                      throw new Error("Network response was not ok");
+                    }
+                    return response.json();
+                  })
+                  .then((data) => {
+                    if (data) {
+                      console.log("utilisatuer deleted successfully");
+                      setUtilisateurs(
+                        utilisateurs.filter(
+                          (user: any) => user.idUser !== idUserToDelete
+                        )
+                      );
+                    } else {
+                      console.error("utilisatuer deletion failed");
+                    }
+                  })
+                  .catch((error) => {
+                    console.error("Error:", error);
+                  });
+              } else {
+                console.error("Tache deletion failed");
+              }
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
   return (
     <div>
@@ -65,20 +127,34 @@ export default function ListUtilisateur() {
             <th scope="col">Nom Utilisateur</th>
             <th scope="col">Username</th>
             <th scope="col">Email</th>
-            <th scope="col" className='text-center' colSpan={2}>Operation</th>
+            <th scope="col" className="text-center" colSpan={2}>
+              Operation
+            </th>
           </tr>
         </thead>
         <tbody>
-          {utilisateurs.map((utilisateur:any, index) => (
+          {utilisateurs.map((utilisateur: any, index) => (
             <tr key={index}>
               <th scope="row">{index + 1}</th>
-              <td>{utilisateur.nom}</td>
+              <td>{utilisateur.name}</td>
               <td>{utilisateur.username}</td>
               <td>{utilisateur.email}</td>
-              <td><Link href={`/Utilisateur/updateUser/${utilisateur.idUser}`} className='btn btn-primary'>Update</Link></td>
-              <td><button className="btn btn-danger" onClick={() => deleteUser(utilisateur.idUser)}>
-                    Delete
-                  </button></td>
+              <td>
+                <Link
+                  href={`/Utilisateur/updateUser/${utilisateur.idUser}`}
+                  className="btn btn-primary"
+                >
+                  Update
+                </Link>
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteUser(utilisateur.idUser)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
